5102220b371b8996b18bf944b02a54870d6a43a8
Syed Kaif <49721304+webdeper@users.noreply.github.com>
2019-09-21 08:31:42 +0530

    added meta tags

--- header.php
+++ header.php
@@ -6,12 +6,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="images/Subtraction 5.png" type="image/gif" sizes="15x15">
+    <meta name="title" content="Dhami Sports | We Create and Organize Sports Events and Tours">
+<meta name="description" content="Dhami Sports is the most experienced and qualified provider of comprehensive travel and incentive packages to major sports events and leisure destinations worldwide.">
+<meta name="keywords" content="dhami sports, sports tour, sports agenct, dhami sports management, sports management, sports organizer, sports events, sports, management, dhami, tour organizer, sports tour organizer, basketball sports events, swimming sports events, sports travel, sports">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <link rel="stylesheet" href="styles/style.css">
     <title>Dhami Sports</title>
+    
 </head>
 
 <body>
